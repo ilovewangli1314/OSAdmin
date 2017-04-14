7784344e3588e839d7d3f057e9a856c1e91f8d6e
JiangGang <18611954314@yeah.net>
2017-04-15 00:37:10 +0800

    log

--- uploads/panel/index.php
+++ uploads/panel/index.php
@@ -1,6 +1,9 @@
 <?php
 require('../include/init.inc.php');
 
+echo date('Y-m-d H:i:s', 1492186704);
+echo date('Y-m-d H:i:s', 1492128000);
+
 $user_info = UserSession::getSessionInfo();
 $menus = MenuUrl::getMenuByIds($user_info['shortcuts']);
 Template::assign('menus', $menus);
