a6b4c83a984f0f33e8ded430bcfde51bd28f9fc3
JiangGang <18611954314@yeah.net>
2017-04-14 19:23:17 +0800

    fix

--- uploads/index.php
+++ uploads/index.php
@@ -2,7 +2,4 @@
 
 error_reporting (E_ALL & ~E_NOTICE);
 
-print_r(111 . '</br>');
-Common::print_r_n("123");
-
-//header("Location:panel/index.php");
+header("Location:panel/index.php");
--- uploads/panel/index.php
+++ uploads/panel/index.php
@@ -1,6 +1,18 @@
-<?php 
+<?php
+
+print_r(111 . '</br>');
+
 require('../include/init.inc.php');
+
+print_r(222 . '</br>');
+
 $user_info = UserSession::getSessionInfo();
+
+print_r(333 . '</br>');
+
 $menus = MenuUrl::getMenuByIds($user_info['shortcuts']);
+
+print_r(444 . '</br>');
+
 Template::assign('menus', $menus);
 Template::display('index.tpl');
