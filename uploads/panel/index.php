7db3bd9c5a9ddbc6fb0b7535b5bce5a362d01be4
JiangGang <18611954314@yeah.net>
2017-04-14 20:42:14 +0800

    fix

--- uploads/panel/index.php
+++ uploads/panel/index.php
@@ -1,15 +1,7 @@
 <?php
 require('../include/init.inc.php');
 
-print_r(222 . '</br>');
-
 $user_info = UserSession::getSessionInfo();
-
-print_r(333 . '</br>');
-
 $menus = MenuUrl::getMenuByIds($user_info['shortcuts']);
-
-print_r(444 . '</br>');
-
 Template::assign('menus', $menus);
 Template::display('index.tpl');
